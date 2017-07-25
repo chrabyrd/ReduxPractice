74d7ae10a1083dea62c166a29bb6cd17b8197369
Christopher <chrabyrd@gmail.com>
2017-07-25 16:35:39 -0700

    Add Router

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import css from './styles/style.styl';
 
 import Main from './components/Main';
+import SinglePhoto from './components/SinglePhoto';
+import PhotoGrid from './components/PhotoGrid';
 
 document.addEventListener('DOMContentLoaded', () => {
-  render(<Main />, document.querySelector('#root'));
+  const router = (
+    <Router history={hashHistory}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={SinglePhoto}></Route>
+      </Route>
+    </Router>
+  )
+
+  render(router, document.querySelector('#root'));
 })
